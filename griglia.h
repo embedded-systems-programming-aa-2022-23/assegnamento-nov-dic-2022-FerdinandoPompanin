f59bc10727dc51d812933b0579fa65101eff5bd3
Ferdinando Pompanin <ferdinando.pompanin@studenti.unipd.it>
2022-12-04 01:32:07 +0100

    creato la prima parte dell' assegnamento, la roba compila, ma devi ancora fare tutto il testing poi ti manca da fare la parte del robot. ciao prof. Reggiani se stai leggendo.

---
+++ griglia.h
@@ -0,0 +1,40 @@
+//griglia.h
+
+#ifndef griglia_h
+#define griglia_h
+
+#include <array>
+#include <set>
+
+class Cella {
+public:
+	Cella(int pos_x, int pos_y);
+	
+	std::array<int,2> pos() const; //restituisce le coordinate della cella
+	
+private:
+	//coordinate della posizione della cella
+	int x_;
+	int y_;
+};	
+	
+	
+class Mappa {
+public:
+	void inserisci_ostacolo(int min_x, int min_y, int max_x, int max_y); //inserimento di ostacolo definito come da specifiche, si può implementare con array o cella
+	void inserisci_robot(Cella posizione);
+	void sposta_robot(Cella prima, Cella dopo);
+	float distanza_cella_vicina(Cella cercata);
+	
+private:
+	std::set<Cella> pos_ostacoli;
+	std::set<Cella> pos_robot;
+};
+
+float distanza_euclidea(const Cella pos_1, const Cella pos_2);
+
+bool operator==(const Cella& c1, const Cella& c2);
+
+bool operator<(const Cella& c1, const Cella& c2);
+
+#endif
